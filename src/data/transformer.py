d48676b9027f0ea0dec5a9d87033208e15610dad
lwkys <hasibuanfirman1404@gmail.com>
2026-05-23 18:18:19 +0700

    feat: add segmentation transform pipelines

---
+++ src/data/transformer.py
@@ -0,0 +1,24 @@
+from src.config import IMAGE_SIZE
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform(image_size=IMAGE_SIZE):
+    return A.Compose(
+        [
+            A.Resize(image_size, image_size),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.RandomRotate90(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    )
+
+def get_valid_transform(image_size=IMAGE_SIZE):
+    return A.Compose(
+        [
+            A.Resize(image_size, image_size),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    )
